6ef48a55aae2894185995c5bde157f693eba0343
Andrew Kolos <andrewrkolos@gmail.com>
2020-12-28 10:30:27 -0500

    patch audit-fix-all script to use renamed pub script

--- audit-fix-all.py
+++ audit-fix-all.py
@@ -8,7 +8,8 @@ dirs = (d.name for d in os.scandir(cwd) if d.is_dir() and d.name[0] != '.')
 
 for d in dirs:
   os.chdir(d)
-  subprocess.run('npm audit fix', shell=True)
+  auditResult = subprocess.run('npm audit fix', shell=True, capture_output=True)
+  if re.search('fixed 0 of 0 vulnerabilities') continue
   with open('package.json', 'r+') as f:
     pattern = re.compile(r'("version": "\d+\.\d+\.)(\d+)(")')
     fileContent = f.read()
@@ -18,7 +19,7 @@ for d in dirs:
     f.seek(0)
     f.write(updatedContent)
     f.truncate()
-  subprocess.run('npm run publish', shell=True)
+  subprocess.run('npm run pub', shell=True)
   os.chdir('..')
 
 
